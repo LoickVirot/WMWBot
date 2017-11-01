593a6f6af6477498ebcdc60648ea14470ae133f8
Loïck Virot <virot.loick@gmail.com>
2017-11-01 11:12:13 +0100

    Fix new member message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -7,8 +7,9 @@ module.exports = function (bot, db, user) {
 
     user.user.createDM()
           .then(function (channel) {
-            channel.send('Bienvenue ' + user.username + ' ! Nous sommes ravis de t\'accueillir parmis nous. Il y a quelques règles à respecter, pour le bien de tous. Je t\'invite à les lire :)');
+            channel.send('Bienvenue ' + user.user.username + ' ! Nous sommes ravis de t\'accueillir parmis nous. Il y a quelques règles à respecter, pour le bien de tous. Je t\'invite à les lire :)');
             rules.showRules(channel, db);
+            channel.send('Si tu as besoin de savoir comment je fonctionne, tape `!help`.');
         })
         .catch(function (err) {
             console.log(err);
