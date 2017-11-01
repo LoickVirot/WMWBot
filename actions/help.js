0e4c06aae4377476bac3cbe669278632762cba72
Loïck Virot <virot.loick@gmail.com>
2017-11-01 10:57:47 +0100

    improve messages with embed

--- actions/help.js
+++ actions/help.js
@@ -1,14 +1,21 @@
+const Discord = require('discord.js');
+
 module.exports = {
   showHelp: function(channel, db) {
-    channel.send("Voici les commandes que je connais :")
     let connection = db.collection('commands');
     connection.find({}).toArray(function (err, commands) {
       if(err) {
         console.error(err);
       } else {
-        commands.map(function(command) {
-          channel.send("- !" + command.command + " : " + command.description);
+        let formattedCommand = commands.map(function(command) {
+          return "- `!" + command.command + "` : " + command.description;
         });
+        let embed = new Discord.RichEmbed();
+        embed.setTitle("Liste des commandes :")
+          .setDescription(formattedCommand)
+          .setColor('BLUE')
+        ;
+        channel.send(embed);
       }
     });
   }
--- actions/rules.js
+++ actions/rules.js
@@ -1,15 +1,21 @@
 const params = require('../config/parameters');
+const Discord = require('discord.js');
 
 module.exports = {
   showRules: function(channel, db) {
     let connection = db.collection('guild');
     connection.findOne({id: params.guildId})
-        .then(function (guild) {
-            channel.send(guild.rules);
-        })
-        .catch(function (err) {
-            channel.send("Erreur");
-            console.error(err);
-        });
+      .then(function (guild) {
+        let embed = new Discord.RichEmbed();
+        embed.setTitle("Règles du serveur")
+          .setDescription(guild.rules)
+          .setColor("BLUE")
+        ;
+        channel.send(embed);
+      })
+      .catch(function (err) {
+        channel.send("Erreur");
+        console.error(err);
+      });
   }
 }
